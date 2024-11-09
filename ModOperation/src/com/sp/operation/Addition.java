3bbd7f12ba821df095434eb63c14bea489bf94f7
satish <satish@localhost>
2024-11-09 19:00:04 +0530

    Developer Git code code check-in first time.

---
+++ ModOperation/src/com/sp/operation/Addition.java
@@ -0,0 +1,18 @@
+package com.sp.operation;
+
+public class Addition {
+
+	public int sum(int x, int y) {
+
+		return x + y;
+	}
+
+	public static void main(String[] args) {
+
+		Addition add = new Addition();
+
+		System.out.println("Mod OPERATION -  ADDITION ::" + add.sum(20, 30));
+
+	}
+
+}
