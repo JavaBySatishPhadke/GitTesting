fb8ff8dad8ab28f13d0c3bf24e2e60cf4981c9f2
satish <satish@localhost>
2024-11-11 09:48:40 +0530

    UPI payment is code changes done.

--- ModOperation/src/com/sp/upi/UPIPayment.java
+++ ModOperation/src/com/sp/upi/UPIPayment.java
@@ -3,7 +3,7 @@ package com.sp.upi;
 public class UPIPayment {
 
 	public String doUPIPayment(String UPIID, float amount) {
-		return "UPI Payment is done";
+		return "UPI Payment is "+ amount +" done";
 	}
 
 }
