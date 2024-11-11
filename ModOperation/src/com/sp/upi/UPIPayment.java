46847dad67b38a5050f29c94916b0aaddfd06ae4
satish <satish@localhost>
2024-11-11 09:52:33 +0530

    TL-  Enhanced the Branch1 code.

--- ModOperation/src/com/sp/upi/UPIPayment.java
+++ ModOperation/src/com/sp/upi/UPIPayment.java
@@ -1,9 +1,17 @@
 package com.sp.upi;
 
+import java.util.Random;
+
 public class UPIPayment {
 
 	public String doUPIPayment(String UPIID, float amount) {
-		return "UPI Payment is "+ amount +" done";
+		return "UPI Payment is " + amount + " done";
+	}
+
+	public double checkBalance(String UPID) {
+
+		return new Random().nextInt(1000);
+
 	}
 
 }
